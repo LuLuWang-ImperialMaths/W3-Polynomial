f405be5770049fc5a735e93d0b652b043cb992b5
LuLuWang-ImperialMaths <97701359+LuLuWang-ImperialMaths@users.noreply.github.com>
2022-01-25 14:40:06 +0000

    equality and addition done test_ file not yet

--- polynomials/polynomials/polynomials.py
+++ polynomials/polynomials/polynomials.py
@@ -1,3 +1,5 @@
+from numbers import Number
+
 class Polynomial:
 
 
@@ -10,6 +12,7 @@ class Polynomial:
         return len(self.coefficients) - 1
     
     def __str__(self): # __str__ to represent the variable
+                       # called by 'print' or 'str'
         
         coefs = self.coefficients
         terms = [] 
@@ -27,3 +30,41 @@ class Polynomial:
                                                                    # 'enumerate' gives 2 loop variables
         return " + ".join(reversed(terms)) or "0" # 'join' but descending # 'reversed' so ascending order
                                                   # " or "0" " so that python gives 0 if coefs[] == 0
+
+    def __repr__(self): # __repr__ : default string representation: ' "" ' with '+'
+
+        return type(self).__name__+ "(" + repr(self.coefficients) + ")" # 'type(self).__name__': class name
+    
+    ## whether 2 objects are actually the same
+    def __eq__(self, other): # other : which we compare to
+
+        return self.coefficients == other.coefficients
+
+    def __add__(self, other): # called by '+' or '.__add__()'
+
+        if isinstance(other, Polynomial):
+
+            common = min(self.degree(), other.degree()) + 1  # num of common terms of 2 polys
+
+            coefs = tuple(a + b for a, b in zip(self.coefficients, other.coefficients))
+                                                            # zip: more iterators 
+                                                            # zip stops when any one of the tuple runs out
+            coefs += self.coefficients[common:] + other.coefficients[common:]
+
+            return Polynomial(coefs)
+
+        # if the 'other' is a more general 'number'
+        elif isinstance(other, Number):
+
+            return  Polynomial((self.coefficients[0] + other,) # (x,) so that it's a one-length tuple 
+                               + self.coefficients[1:])
+
+        else:
+
+            return NotImplemented # A special value returned in the case where the addition makes no sense
+
+    def __radd__(self, other): # reverse the order of addition cause it's commutative
+
+        return self.__add__(other)
+
+        
